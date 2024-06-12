8c2429b82dc416bb820a85d79c1d44f4929275f4
Maximus019BG <maxralev@abv.bg>
2024-06-12 22:25:35 +0300

    add:robot code base

--- MBots/M1/M1.py
+++ MBots/M1/M1.py
@@ -3,17 +3,21 @@ def leftTurn(time: number):
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     control.wait_micros(time * 1000000)
     maqueen.motor_stop(maqueen.Motors.ALL)
+    
 def rightTurn(time2: number):
     maqueen.motor_stop(maqueen.Motors.ALL)
     maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
     control.wait_micros(time2 * 1000000)
     maqueen.motor_stop(maqueen.Motors.ALL)
+
 def forward(time3: number):
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 236)
     control.wait_micros(time3 * 1000000)
     maqueen.motor_stop(maqueen.Motors.ALL)
+
 basic.show_icon(IconNames.PITCHFORK)
+
 forward(2)
 rightTurn(0.6)
 forward(1)
