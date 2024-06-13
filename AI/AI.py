51a8522195f2222a46550de70a8d4f1b4f35536a
Maximus019BG <maxralev@abv.bg>
2024-06-13 21:41:28 +0300

    fix:Ai camera + robot Ai code more + robot fixes radio and more

--- AI/AI.py
+++ AI/AI.py
@@ -1,10 +1,13 @@
+radio.set_group(1)
 huskylens.init_i2c()
 huskylens.init_mode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 
 def on_forever():
     huskylens.request()
     if huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        radio.send_number(0)
         basic.show_icon(IconNames.HEART)
     if huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        radio.send_number(1)
         basic.show_icon(IconNames.HAPPY)
 basic.forever(on_forever)
--- MBots/M1/M1.py
+++ MBots/M1/M1.py
@@ -1,33 +1,34 @@
+def on_received_number(receivedNumber):
+    if receivedNumber == 0:
+        basic.show_icon(IconNames.HEART)
+    elif receivedNumber == 1:
+        basic.show_icon(IconNames.HAPPY)
+    else:
+        basic.show_icon(IconNames.NO)
+radio.on_received_number(on_received_number)
+
 def leftTurn(time: number):
     maqueen.motor_stop(maqueen.Motors.ALL)
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     control.wait_micros(time * 1000000)
     maqueen.motor_stop(maqueen.Motors.ALL)
-    
 def rightTurn(time2: number):
     maqueen.motor_stop(maqueen.Motors.ALL)
     maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
     control.wait_micros(time2 * 1000000)
     maqueen.motor_stop(maqueen.Motors.ALL)
-
+    basic.show_icon(IconNames.PITCHFORK)
 def forward(time3: number):
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 236)
     control.wait_micros(time3 * 1000000)
     maqueen.motor_stop(maqueen.Motors.ALL)
-
-basic.show_icon(IconNames.PITCHFORK)
-
-forward(2)
-rightTurn(0.6)
-forward(1)
-control.wait_micros(5 * 1000000)
-forward(1)
-leftTurn(0.6)
-forward(1)
-leftTurn(0.6)
-forward(0.5)
+radio.set_group(1)
 
 def on_forever():
     pass
 basic.forever(on_forever)
+
+def on_forever2():
+    pass
+basic.forever(on_forever2)
